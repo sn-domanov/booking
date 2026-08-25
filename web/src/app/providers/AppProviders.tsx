2bd836f83a52c36925d09f9277a5df2fa3573768
Sergei Domanov <sdomanov.py@gmail.com>
2026-08-25 15:58:02 +0000

    feat(web): add theme provider

--- web/src/app/providers/AppProviders.tsx
+++ web/src/app/providers/AppProviders.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import queryClient from "./query/queryClient";
+import ThemeProvider from "./ThemeProvider";
 
 type AppProvidersProps = {
   children: ReactNode;
@@ -11,11 +12,18 @@ type AppProvidersProps = {
 
 function AppProviders({ children }: AppProvidersProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryClientProvider client={queryClient}>
+        {children}
 
-      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
-    </QueryClientProvider>
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
